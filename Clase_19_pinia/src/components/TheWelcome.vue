06559a8266ecf298baf7790f1866b00a13d2510c
Mateo Loholaberry <mateoloholaberry@gmail.com>
2024-08-16 19:43:15 -0300

    Subiendo clase 19

---
+++ Clase_19_pinia/src/components/TheWelcome.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>{{ store.count }} X 2 = {{ store.doubleCount }}</h1>
+    <button @click="store.increment(1)">Incrementar</button>
+</template>
+
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/counter';
+const store = useCounterStore();
+</script>
