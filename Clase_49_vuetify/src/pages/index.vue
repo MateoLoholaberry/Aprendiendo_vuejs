b27d7f21578b431679c40f1594d2846d64d3e192
MateoLoholaberry <mateoloholaberry@gmail.com>
2024-10-27 16:50:30 -0300

    Subiendo clase 49

---
+++ Clase_49_vuetify/src/pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-sheet class="bg-deep-purple pa-12" rounded>
+    <v-card class="mx-auto px-6 py-8" max-width="344">
+      <v-form>
+        <v-text-field class="mb-2" label="Email" clearable></v-text-field>
+
+        <v-text-field
+          label="Password"
+          placeholder="Enter your password"
+          clearable
+        ></v-text-field>
+
+        <br />
+
+        <v-btn
+          color="success"
+          size="large"
+          type="submit"
+          variant="elevated"
+          block
+        >
+          Sign In
+        </v-btn>
+      </v-form>
+    </v-card>
+  </v-sheet>
+</template>
+
+<script lang="ts" setup>
+//
+</script>
+
+<style scoped>
+.red {
+  background-color: red;
+}
+.blue {
+  background-color: blue;
+}
+.green {
+  background-color: green;
+}
+</style>
